eceb37d2c33d8d52228c39fbcf5e91b1400d143f
Bhushan Patil <bhupatil0001@gmail.com>
2021-05-30 12:01:10 +0530

    First commit

---
+++ src/test/java/com/finovate/GoogleKeepReplicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finovate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleKeepReplicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
